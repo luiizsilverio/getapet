1a5eca793520b59f5efe4efafd35d32efa87938d
luiizsilverio <luiiz.silverio@gmail.com>
2022-08-07 17:30:14 -0300

    defining pages and first components

---
+++ frontend/src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <section>
+      <footer>
+        <p><span className="bold">Get a Pet</span> &copy; 2022</p>
+      </footer>
+    </section>
+  )
+}
